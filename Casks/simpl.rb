48fc1cbd64403a2bf720ba57613e20b65b658cae
Emiel Wiedijk <me@aimileus.nl>
2021-09-02 15:27:39 +0200

    Fix style SIMPL cask

--- Casks/simpl.rb
+++ Casks/simpl.rb
@@ -2,9 +2,10 @@ cask "simpl" do
   version "2.3.2"
   sha256 "2dc2e397993d271373c303f29aab2f0a2935d3af5873cdfbc8b2c83fb7a11331"
 
-  url "https://github.com/aimileus/simpl/releases/download/v2.3.2/SIMPL.zip"
+  url "https://github.com/aimileus/simpl/releases/download/v#{version}/SIMPL.zip"
+  name "SIMPL"
   desc "Academic tool for computer organisation and architecture"
-  homepage "https://fnwi.uva.nl"
+  homepage "https://github.com/aimileus/simpl"
 
   app "SIMPL.app"
 end
