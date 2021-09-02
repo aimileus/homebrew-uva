686a864d2198773125b22e93d14b17b7f82403eb
Emiel Wiedijk <me@aimileus.nl>
2021-09-02 15:21:03 +0200

    Add SIMPL cask

---
+++ Casks/simpl.rb
@@ -0,0 +1,10 @@
+cask "simpl" do
+  version "2.3.2"
+  sha256 "2dc2e397993d271373c303f29aab2f0a2935d3af5873cdfbc8b2c83fb7a11331"
+
+  url "https://github.com/aimileus/simpl/releases/download/v2.3.2/SIMPL.zip"
+  desc "Academic tool for computer organisation and architecture"
+  homepage "https://fnwi.uva.nl"
+
+  app "SIMPL.app"
+end
