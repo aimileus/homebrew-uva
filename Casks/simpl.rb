d58d3f82fdcd9ec5cdd2b462ed725dcbd280f3bb
Jonas van der Schaaf <niet@jonasvanderschaaf.nl>
2021-09-05 21:33:26 +0200

    Use newer version of SIM-PL .app file
    Update the logo to comply with Apple style guide

--- Casks/simpl.rb
+++ Casks/simpl.rb
@@ -1,6 +1,6 @@
 cask "simpl" do
-  version "2.3.2-2"
-  sha256 "cf575aae095f185a913b0409b789dc0b9aea289d815aa89d096b253bc01ff638"
+  version "2.3.2-3"
+  sha256 "df335beb3b88d4767ef5a4c7079f5272938b8d3277fdbeaa791f9077c4c24092"
 
   url "https://github.com/aimileus/simpl/releases/download/v#{version}/SIMPL-#{version}.zip"
   name "SIMPL"
